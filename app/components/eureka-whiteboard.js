81607839d68acdc77464c05380cab0b67b1d0071
Han Loong Liauw <hanloongliauw@gmail.com>
2015-04-10 17:10:13 +1000

    added more tools

--- app/components/eureka-whiteboard.js
+++ app/components/eureka-whiteboard.js
@@ -4,12 +4,19 @@ export default Ember.Component.extend({
   classNameBindings: ['graphPaper'],
   stage: null,
   shape: null,
+  tempShape: null,
   wrapper: null,
   graphPaper: false,
+  fillShape: false,
 
   drawing: false,
   oldX: null,
   oldY: null,
+  startX: null,
+  startY: null,
+  endX: null,
+  endY: null,
+
   color: '#000',
   owidth: 800,
   oheight: 600,
@@ -23,6 +30,7 @@ export default Ember.Component.extend({
     var stage = new createjs.Stage("demoCanvas");
     var shape = new createjs.Shape();
     var wrapper = new createjs.Container();
+    var tempShape = new createjs.Shape();
 
     createjs.Ticker.addEventListener("tick", stage);
 
@@ -30,8 +38,9 @@ export default Ember.Component.extend({
 
     stage.addChild(wrapper);
     wrapper.addChild(shape);
+    stage.addChild(tempShape);
 
-    createjs.Touch.enable(stage);
+    createjs.Touch.enable(stage, false, true);
 
     // Setup mouse listeners
     stage.on("stagemousemove", this.mouseover.bind(this));
@@ -42,6 +51,7 @@ export default Ember.Component.extend({
     this.set('stage', stage);
     this.set('shape', shape);
     this.set('wrapper', wrapper);
+    this.set('tempShape', tempShape);
     this.stage.update();
   },
 
@@ -63,30 +73,84 @@ export default Ember.Component.extend({
     this.set('drawing', true);
     this.set('oldX', evt.stageX);
     this.set('oldY', evt.stageY);
+    this.set('startX', evt.stageX);
+    this.set('startY', evt.stageY);
+
+    this.get('tempShape').graphics.clear();
+    this.get('stage').update();
   },
   stopDrawing: function(evt) {
+
+    if (this.get('drawing') === true) {
+      var shape = this.get('shape');
+      var w = evt.stageX - this.get('startX');
+      var h = evt.stageY - this.get('startY');
+
+      if (this.get('current_tool') === 'circle') {
+        if(this.get('fillShape')) {
+          shape.graphics.beginFill(this.get('color')).drawEllipse(this.get('startX'), this.get('startY'), w, h);
+        } else {
+          shape.graphics.beginStroke(this.get('color')).drawEllipse(this.get('startX'), this.get('startY'), w, h);
+        }
+      }
+      if (this.get('current_tool') === 'square') {
+        if(this.get('fillShape')) {
+          shape.graphics.beginFill(this.get('color')).drawRect(this.get('startX'), this.get('startY'), w, h);
+        } else {
+          shape.graphics.beginStroke(this.get('color')).drawRect(this.get('startX'), this.get('startY'), w, h);
+        }
+      }
+
+      this.get('wrapper').updateCache("source-over");
+    }
+
     this.set('drawing', false);
     this.set('oldX', null);
     this.set('oldY', null);
+    this.set('endX', evt.stageX);
+    this.set('endY', evt.stageY);
+
+    this.get('tempShape').graphics.clear();
   },
 
   mouseover: function(evt) {
-    if (this.get('drawing') == true) {
-      var erase = this.get('current_tool') === 'eraser'
+    if (this.get('drawing') === true) {
+      var erase = this.get('current_tool') === 'eraser';
       if(this.get('oldX')) {
         var drawing = this.get('shape');
         var size = this.get('size');
-        if (erase) {
-          size = size*5;
+
+        if (erase) { size = size*5; }
+
+        if (this.get('current_tool') === 'pen' || this.get('current_tool') === 'eraser') {
+          drawing.graphics.ss(size, "round").s(this.get('color'));
+          drawing.graphics.mt(this.get('oldX'), this.get('oldY'));        
+          drawing.graphics.lt(evt.stageX, evt.stageY);
         }
 
-        drawing.graphics.ss(size, "round").s(this.get('color'));
-        drawing.graphics.mt(this.get('oldX'), this.get('oldY'));        
-        drawing.graphics.lt(evt.stageX, evt.stageY);
+        var tempShape = this.get('tempShape');
+        var w = evt.stageX - this.get('startX');
+        var h = evt.stageY - this.get('startY');
+
+        if (this.get('current_tool') === 'circle') {
+          if(this.get('fillShape')) {
+            tempShape.graphics.clear().beginFill(this.get('color')).drawEllipse(this.get('startX'), this.get('startY'), w, h);
+          } else {
+            tempShape.graphics.clear().beginStroke(this.get('color')).drawEllipse(this.get('startX'), this.get('startY'), w, h);
+          }
+        }
+        if (this.get('current_tool') === 'square') {
+          if(this.get('fillShape')) {
+            tempShape.graphics.clear().beginFill(this.get('color')).drawRect(this.get('startX'), this.get('startY'), w, h);
+          } else {
+            tempShape.graphics.clear().beginStroke(this.get('color')).drawRect(this.get('startX'), this.get('startY'), w, h);
+          }
+        }
       }
+
       this.set('oldX', evt.stageX);
       this.set('oldY', evt.stageY);
-
+      this.get('stage').update();
       this.get('wrapper').updateCache(erase?"destination-out":"source-over");
       this.get('shape').graphics.clear();
     }
